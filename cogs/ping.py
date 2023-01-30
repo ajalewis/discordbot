42dc0d6f1b0e587759f15f40e6bd0b7206525c0c
Alexander Lewis <alexander.lewis@neds.com>
2023-01-30 15:50:19 +1000

    Refactor and Change to Cogs

---
+++ cogs/ping.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%d-%m-%Y %H:%M:%S')
+
+class Ping(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command()
+  async def ping(self, ctx):
+    await ctx.send("Pong")
+    logging.info(f'{ctx.author} sent !ping')
+    
+async def setup(bot):
+  await bot.add_cog(Ping(bot))
--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 # Discord bot for Zealots.
 # By alexanderashworthlewis@gmail.com
 
-import bot
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import logging
+import os
+import asyncio
 
-if __name__ == '__main__':
-    bot.run_discord_bot()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%d-%m-%Y %H:%M:%S')
+
+load_dotenv()
+token = os.getenv('token_dev')
+activity = discord.Activity(type=discord.ActivityType.watching, name="Zealots Server")
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents, activity=activity)
+
+@bot.event
+async def on_ready():
+  logging.info(f'{bot.user} is up and running')
+
+async def load_extensions():  
+  for f in os.listdir("./cogs"):
+    if f.endswith(".py"):
+      await bot.load_extension("cogs." + f[:-3])
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
+
+asyncio.run(main())
